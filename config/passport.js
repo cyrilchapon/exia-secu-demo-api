061095e4658d3beb36d1135aad06ffa3a91d468f
Cyril CHAPON <cyril.chapon@live.com>
2016-03-11 22:22:58 +0100

    Fixed jwt behavior

--- config/passport.js
+++ config/passport.js
@@ -8,10 +8,10 @@ var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 
 var EXPIRES_IN_SECONDS = 60 * 60 * 24;
-var SECRET = process.env.tokenSecret || "4ukI0uIVnB3iI1yxj646fVXSE3ZVk4doZgz6fTbNg7jO41EAtl20J5F7Trtwe7OM";
+var SECRET = process.env.tokenSecret || "84fsdfaz984f2d1fsd64fdsv21v6s4sd8v4sdv2sd84fzefsdfsdf5sd48zeffsdfs";
 var ALGORITHM = "HS256";
-var ISSUER = "shqdow";
-var AUDIENCE = "shqdow";
+var ISSUER = "exia.cesi.secu.demo";
+var AUDIENCE = "exia.cesi.secu.demo";
 
 /**
  * Triggers when user authenticates via local strategy
@@ -38,7 +38,16 @@ function _onLocalStrategyAuth(email, password, next) {
  */
 function _onJwtStrategyAuth(payload, next) {
   var user = payload.user;
-  return next(null, user);
+  
+  if (!user) return next(null, false);
+  
+  User.findOne({email: user.email}).exec(function (error, user) {
+    if (error) return next(error, false);
+
+    if (!user) return next(null, false);
+    
+    return next(null, user);
+  });
 }
 
 passport.use(new LocalStrategy({
